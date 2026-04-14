6ab3f36ae2c3e5b3daf83218d1f0555ea282878b
ariel raymundo <ariraymundo98@gmail.com>
2026-04-14 12:40:26 -0700

    theater reservation

---
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class HighScoreWins
+{
+    static void main()
+    {
+
+        IO.println(String.format("Hello and welcome!"));
+
+    }
+}
